b2fb4968af5a3d3284e7ef970115df0cdfb7712c
HemaPriya Dharshini <hemahpd07@gmail.com>
2021-10-26 18:23:09 +0530

    Admin panel customized

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,7 +3,14 @@ from django.contrib import admin
 from accounts.models import BookForm, NewUser
 
 # Register your models here.
-admin.site.register(NewUser)
-admin.site.register(BookForm)
 
 
+admin.site.site_header="MADURAINEEDS ADMINISTRATION PAGE"
+class NewUserAdmin(admin.ModelAdmin):
+    list_display= ("username","email","pwd","gender")
+
+class BookFormAdmin(admin.ModelAdmin):
+    list_display=("name","specialisation","phnno","addressline1","mainarea","pincode")
+
+admin.site.register(NewUser,NewUserAdmin)
+admin.site.register(BookForm,BookFormAdmin)
--- worker/admin.py
+++ worker/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
+from django.contrib.admin.options import ModelAdmin
 from worker.models import Worker,Job
 
 
 # Register your models here.
-admin.site.register(Worker)
-admin.site.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display=("jobid","jobtype")
+
+class WorkerAdmin(admin.ModelAdmin):
+    list_display=("jobid","wname","wphnno","gender","experience","location","specialisation")
+
+admin.site.register(Worker,WorkerAdmin)
+admin.site.register(Job,JobAdmin)
